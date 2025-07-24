8b3f57f4dbe45c99a7d5ae02a39668890d79b350
DongHyeon Lim <dlaehdgus23@naver.com>
2025-07-25 06:32:12 +0900

    refactor : import 누락

--- src/test/java/kr/hhplus/be/server/balance/BalanceDomainServiceTest.java
+++ src/test/java/kr/hhplus/be/server/balance/BalanceDomainServiceTest.java
@@ -12,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
