b5e8317c0136fb8feac2817b23ea7a47547d3c4c
DongHyeon Lim <dlaehdgus23@naver.com>
2025-08-08 05:34:16 +0900

    refactor : 쿠폰 동시성 제어 구현 및 트랜잭션 적용

--- src/main/java/kr/hhplus/be/server/coupon/application/CouponService.java
+++ src/main/java/kr/hhplus/be/server/coupon/application/CouponService.java
@@ -1,20 +1,18 @@
 package kr.hhplus.be.server.coupon.application;
 
-import jakarta.transaction.Transactional;
 import kr.hhplus.be.server.coupon.api.dto.request.CouponIssueRequestDto;
 import kr.hhplus.be.server.coupon.api.dto.response.CouponUseResponseDto;
 import kr.hhplus.be.server.coupon.api.dto.response.UserCouponResponseDto;
-import kr.hhplus.be.server.coupon.domain.repository.CouponRepository;
 import kr.hhplus.be.server.coupon.domain.service.CouponDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class CouponService {
 
     private final CouponDomainService couponDomainService;
-    private final CouponRepository couponRepository;
 
     @Transactional
     public UserCouponResponseDto issue(CouponIssueRequestDto request) {
--- src/main/java/kr/hhplus/be/server/coupon/infrastructure/persistence/jpa/CouponJpaRepository.java
+++ src/main/java/kr/hhplus/be/server/coupon/infrastructure/persistence/jpa/CouponJpaRepository.java
@@ -1,8 +1,14 @@
 package kr.hhplus.be.server.coupon.infrastructure.persistence.jpa;
 
+import jakarta.persistence.LockModeType;
 import kr.hhplus.be.server.coupon.domain.model.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+
+import java.util.Optional;
 
 
 public interface CouponJpaRepository extends JpaRepository<Coupon,Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<Coupon> findWithLockById(Long couponId);
 }
